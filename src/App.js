d443d1c3dcdbd48335cb10ac6c118691ea429219
hank <matheus_bueno10@hotmail.com>
2021-09-27 14:35:15 -0300

    Adicionado resolu da atv

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FruitsPage from "./Components/Fruit";
+
+function App() {
+  return (
+    <div className="App">
+      <FruitsPage></FruitsPage>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Fruit/index.js
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+
+const FruitsPage = () => {
+  //acessando o estado global e pegando o state fruits
+  const fruits = useSelector((state) => state.fruits);
+
+  return (
+    <div>
+      {fruits.map((fruit) => (
+        <p key={fruit}> {fruit} </p>
+      ))}
+    </div>
+  );
+};
+
+export default FruitsPage;
